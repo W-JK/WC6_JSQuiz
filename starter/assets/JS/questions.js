6c1e7d780cb06a17291b1cb36cd2e6138fc2ff50
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-20 19:03:37 +0000

    questions and answers skeleton

---
+++ starter/assets/JS/questions.js
@@ -0,0 +1,30 @@
+console.log("Quiz Questions")
+// Question test skeleton 
+    /* 
+      {
+    title: 'First question',
+    code: '',
+    choices: ['1st answer', '2nd answer','3rd one','last one'],
+    answer: 'correct answer'
+    },
+
+    */
+
+const questions = [
+    {
+    title: 'First question',
+    code: '',
+    choices: ['1st answer', '2nd answer','3rd one','last one'],
+    answer: 'correct answer'
+    },
+
+    {
+    title: 'First question',
+    code: '',
+    choices: ['1st answer', '2nd answer','3rd one','last one'],
+    answer: 'correct answer'
+            },
+
+// add questions 
+
+]
