df43d7bea9dee5664c1918e7e42ce4cea19e255a
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-22 21:57:44 +0000

    highscore logic added

---
+++ starter/assets/JS/highscores.js
@@ -0,0 +1,20 @@
+const highScoresList = document.getElementById('highScoresList');
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+// test point - high scores list: console.log(highScores);
+
+/* ? replaced ?
+highScores.map(score => {
+    // console.log('<li> ${score.name}-${score.score} </li>');
+    console.log('li'); // ? how to display li in JS as list 
+    
+}); */ 
+
+// ---- - high scores to be fixed ... 
+highScoresList.innerHTML = 
+    highScores
+        .map(score =>{
+            return score.name + " - " + score.score ;
+            
+            // '<li class="high-score">${score.name}-${score.score}</li> ';
+        })
+        // .join("");
