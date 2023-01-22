3e5e0a743938202825b6fcda36eecd2bbaa59699
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-22 19:08:34 +0000

    game variables + temp question set (skeleton)

--- starter/assets/JS/game.js
+++ starter/assets/JS/game.js
@@ -1,1 +1,54 @@
 console.log("start game")
+    // variables 
+
+    // -------------------------- 
+    let currentQuestion ={};
+    let acceptingAnswer = true;
+    let score = 0;
+    let questionCounter = 0;
+    let availableQuestions = [];
+
+
+    // ------------- question set ----------------- //
+let questions = [
+    {
+        question: "question 1 text - text ",
+        choice1: "q1 Choice1",
+        choice2: "q1 Choice2",
+        choice3: "q1 Choice3",
+        choice4: "q1 Choice4",
+        answer: 1     
+
+    },
+
+    {
+        question: "question 2 text - text ",
+        choice1: "q2 Choice1",
+        choice2: "q2 Choice2",
+        choice3: "q2 Choice3",
+        choice4: "q2 Choice4",
+        answer: 2    
+
+    },
+
+    {
+        question: "question 3 text - text ",
+        choice1: "q3 Choice1",
+        choice2: "q3 Choice2",
+        choice3: "q3 Choice3",
+        choice4: "q3Choice4",
+        answer: 3   
+
+    },
+
+    {
+        question: "question 4 text - text ",
+        choice1: "q4 Choice1",
+        choice2: "q4 Choice2",
+        choice3: "q4 Choice3",
+        choice4: "q4 Choice4",
+        answer: 4     
+
+    }
+]; 
+    
