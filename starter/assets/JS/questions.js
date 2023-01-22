b66bde8e2cfe6b5e720ff41ac1917696ab1dbcbd
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-22 16:46:00 +0000

    questions set - expand test set

--- starter/assets/JS/questions.js
+++ starter/assets/JS/questions.js
@@ -1,30 +1,58 @@
 console.log("Quiz Questions")
 // Question test skeleton 
-    /* 
-      {
-    title: 'First question',
-    code: '',
-    choices: ['1st answer', '2nd answer','3rd one','last one'],
-    answer: 'correct answer'
-    },
 
-    */
+/*   {
+      question: "question 1 text - text ",
+      choice1: "q1 Choice1",
+      choice2: "q1 Choice2",
+      choice3: "q1 Choice3",
+      choice4: "q1 Choice4",
+      answer: 1     
 
-const questions = [
-    {
-    title: 'First question',
-    code: '',
-    choices: ['1st answer', '2nd answer','3rd one','last one'],
-    answer: 'correct answer'
-    },
+  }, */
 
-    {
-    title: 'First question',
-    code: '',
-    choices: ['1st answer', '2nd answer','3rd one','last one'],
-    answer: 'correct answer'
-            },
+// ------------- question set ----------------- //
+let questions = [
+  {
+      question: "question 1 text - text ",
+      choice1: "q1 Choice1",
+      choice2: "q1 Choice2",
+      choice3: "q1 Choice3",
+      choice4: "q1 Choice4",
+      answer: 1     
+
+  },
+
+  {
+      question: "question 2 text - text ",
+      choice1: "q2 Choice1",
+      choice2: "q2 Choice2",
+      choice3: "q2 Choice3",
+      choice4: "q2 Choice4",
+      answer: 2    
+
+  },
+
+  {
+      question: "question 3 text - text ",
+      choice1: "q3 Choice1",
+      choice2: "q3 Choice2",
+      choice3: "q3 Choice3",
+      choice4: "q3Choice4",
+      answer: 3   
+
+  },
+
+  {
+      question: "question 4 text - text ",
+      choice1: "q4 Choice1",
+      choice2: "q4 Choice2",
+      choice3: "q4 Choice3",
+      choice4: "q4 Choice4",
+      answer: 4     
+
+  }
+];
 
 // add questions 
 
-]
