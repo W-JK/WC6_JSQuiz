9f11a8b84c72a35711b389b1d6b5729b3d234aa1
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-23 23:50:10 +0000

    other - high score list - End Of Day

--- starter/assets/JS/highscores.js
+++ starter/assets/JS/highscores.js
@@ -7,15 +7,28 @@ highScores.map(score => {
     // console.log('<li> ${score.name}-${score.score} </li>');
     console.log('li'); // ? how to display li in JS as list 
     
-}); */ 
+}); */
+
+
 
 // ---- - high scores to be fixed ... 
 highScoresList.innerHTML = 
     highScores
-        .map(score =>{
-            
-            return   score.name + " - " + score.score;
+        .map(score =>{      
+
+          
+            return  score.name + " - " + score.score +"  " ;
+
+           
             
             // '<li class="high-score">${score.name}-${score.score}</li> ';
         })
         // .join("");
+
+    
+
+
+
+
+
+   
