67b6660e121c6de53a1f938bc4b2b26cef48eb19
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-23 19:02:09 +0000

    high score bug fixed

--- starter/assets/JS/end.js
+++ starter/assets/JS/end.js
@@ -6,9 +6,13 @@ const mostRecentScore = localStorage.getItem('mostRecentScore');
 
 
 // High scores saving 
-  localStorage.setItem("highScores",JSON.stringify([])); //console test:
+   // localStorage.setItem("highScores", JSON.stringify([])); 
+   // console.log(localStorage.getItem('highScores')); //console test:  
+
 const highScores = JSON.parse( localStorage.getItem("highScores")) || []; // get high scores or if null - "get empty array"
+const MAX_HIGH_SCORES = 5;
 console.log(highScores); // test point: saving high scores
+
 console.log(JSON.parse(localStorage.getItem("highScores")));
 
 finalScore.innerText = mostRecentScore // final score display
@@ -31,12 +35,16 @@ saveHighScore = e => {
         score: mostRecentScore,
         name: username.value
     }; 
+
+    console.log(score); // testpoint
+
     highScores.push(score);
+    console.log(highScores) //testpoint
     highScores.sort( (a,b) => b.score - a.score ) // return highest to lowest if b is higher put b before a (simplified: no return or brackets)
     highScores.splice(5); // cutting of to the first 5
 
     localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('./end.html'); // ? error with saving scores 
+    window.location.assign('./index.html'); // ? error with saving scores 
 
     console.table(highScores);
 
