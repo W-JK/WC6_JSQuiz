041d740d45b811bd1b2c6dcb341c10165e2d00d0
Wojtek <118207811+W-JK@users.noreply.github.com>
2023-01-24 17:08:27 +0000

    high scores js cosmetics +  hidding test point

--- starter/assets/JS/highscores.js
+++ starter/assets/JS/highscores.js
@@ -1,29 +1,16 @@
 const highScoresList = document.getElementById('highScoresList');
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores); // test point - high scores list:
-
-/* ? replaced ?
-highScores.map(score => {
-    // console.log('<li> ${score.name}-${score.score} </li>');
-    console.log('li'); // ? how to display li in JS as list 
-    
-}); */
+// test point - high scores list: console.log(highScores); 
 
 
-
-// ---- - high scores to be fixed ... 
+// ---- - high scores 
 highScoresList.innerHTML = 
     highScores
-        .map(score =>{      
-
-          
-            return  score.name + " - " + score.score +"  " ;
-
-           
+        .map(score =>{             
+            return  score.name + " - " + score.score +"  " ;                 
             
-            // '<li class="high-score">${score.name}-${score.score}</li> ';
         })
-        // .join("");
+        
 
         
 
